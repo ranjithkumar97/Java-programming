5f03b710669858bee65c0174e87e862d94ac9f6b
ranjith kumar <ranjithkumarrks101@gmail.com>
2018-12-23 21:05:44 +0530

    person pojo t

---
+++ Core java/src/com/util/collections/person.java
@@ -0,0 +1,20 @@
+package com.util.collections;
+
+public class person {
+
+	int avg;
+	String name;
+	public int getAvg() {
+		return avg;
+	}
+	public void setAvg(int avg) {
+		this.avg = avg;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
