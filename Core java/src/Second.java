fdc2211a7cbf627055486fb600d68aa3a61f41ba
ranjith kumar <ranjithkumarrks101@gmail.com>
2018-12-23 21:02:58 +0530

    Arraylist class

--- Core java/src/Second.java
+++ Core java/src/Second.java
@@ -1,4 +1,7 @@
 
 public class Second {
-
-}
+	public static void main(String args[]){  
+		String name="javatpoint";  
+		char ch=name.charAt(4);//returns the char value at the 4th index  
+		System.out.println(ch);  
+		}}  
